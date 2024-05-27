4723db05d52412d11f3a2fae29f55709b1f5801d
charz185 <80986803+charz185@users.noreply.github.com>
2024-05-27 11:08:54 -0400

    scrawler movement rework - Start of radiation ability of player

--- Src/Scripts/Enemies/Scrawler/Scrawler.gd
+++ Src/Scripts/Enemies/Scrawler/Scrawler.gd
@@ -2,32 +2,33 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var nearby = []
-
+#Used for freeing the scrawler from a stuck position
+var lastPosition = self.global_position
+var lastRotation = self.global_rotation
+var stuck = false
 var attacking = false
 
 func _physics_process(delta):
 	# Add the gravity.
-	if len(nearby) > 1:
-		self.global_rotation += (self.get_angle_to(nearby[0].global_position)-global_rotation)/2
-			
-		if angle_difference(get_angle_to(nearby[0].global_position),self.global_rotation) > -.1 and angle_difference(get_angle_to(nearby[0].global_position),self.global_rotation) < .1:
-			if global_position.distance_to(nearby[0].global_position) < 60:
-				attacking = true
-				$ScrawlerMeleeArea/MeleeParticles.emitting = true
-
-	velocity = Vector2(10,0).rotated(global_rotation)
+	if len(nearby) > 0 and !stuck:
+		self.global_rotation += (self.get_angle_to(nearby[0].global_position))*.08
+		velocity = Vector2(8,0).rotated(global_rotation)
 	move_and_collide(velocity)
+	
+#180 rotation if stuck in same position
+func _check_stuck(last_position):
+	if self.global_position.distance_to(last_position) < 20:
+		stuck = true
+		self.global_rotation = lastRotation+PI
+		velocity = Vector2(5,0).rotated(global_rotation)
 
 #Nearby enters
 func _on_area_2d_body_entered(body):
 	if body not in nearby:
 		nearby.append(body)
-
+		
 #Nearby exits
 func _on_area_2d_body_exited(body):
 	if body in nearby:
@@ -35,6 +36,24 @@ func _on_area_2d_body_exited(body):
 
 
 func _on_scrawler_melee_area_body_entered(body):
+	attacking = true
+	$ScrawlerMeleeArea/MeleeParticles.emitting = true
+	$ScrawlerMeleeArea/ColorRect.color = Color8(255,0,0,255)
+
+
+
+func _on_scrawler_melee_area_body_exited(body):
 	if attacking:
+		attacking = false
+		$ScrawlerMeleeArea/ColorRect.color = Color8(255,255,255,255)
 		pass
-		#body.Health -= 1
+
+
+func _on_last_pos_rot_timer_timeout():
+	if self.global_position.distance_to(lastPosition) < 10:
+		_check_stuck(lastPosition)
+	else:
+		stuck = false
+	lastPosition = self.global_position
+	lastRotation = self.global_rotation
+	
---
+++ Src/Scripts/Player/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+var radiating = false
+var radiatingTimeLeft = 0
+var inradiation = []
+func _physics_process(delta):
+	if Input.is_action_just_released("radiation_Blast") and not radiating:
+		radiating = true
+		radiatingTimeLeft = 40
+		$RadiationBlastArea/CollisionShape2D.disabled = false
+		$RadiationBlastArea/RadiationParticles.emitting = true
+
+func _on_radiation_blast_area_body_entered(body):
+	if body not in inradiation:
+		inradiation.append(body)
+
+func _on_radiation_move_timer_timeout():
+	for i in inradiation:
+		
+	if radiating and radiatingTimeLeft > 0:
+			radiatingTimeLeft -= 1
+			print(int(radiatingTimeLeft/.1))
+			$RadiationBlastArea/CollisionShape2D.shape.radius = int(radiatingTimeLeft/.1)
+			$RadiationBlastArea/RadiationParticles.emission_sphere_radius = int(radiatingTimeLeft/.1) 
+			if radiatingTimeLeft <= 0:
+				radiating = false
+	else:
+		$RadiationBlastArea/RadiationParticles.emitting = false
+		$RadiationBlastArea/CollisionShape2D.disabled = true
+
+
+func _on_radiation_blast_area_body_exited(body):
+	if body in inradiation:
+		inradiation.erase(body)
