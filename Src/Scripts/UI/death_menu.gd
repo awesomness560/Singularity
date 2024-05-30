7fa7d610e7eb7e0b72e580ff0b7cfa3de5973856
awesomness560 <appsforyou6@gmail.com>
2024-05-30 11:26:11 -0500

    Finished death menu

--- Src/Scripts/UI/death_menu.gd
+++ Src/Scripts/UI/death_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var mainMenu : PackedScene
 @export var enemyScoreLabel : Label
 @export var sizeScoreLabel : Label
 
@@ -8,3 +9,9 @@ func _on_visibility_changed():
 		#TODO: Need to add a score based on the time taken
 		enemyScoreLabel.text = "Enemy score: " + str(GlobalVars.enemyScore)
 		sizeScoreLabel.text = "Size score: " + str(GlobalVars.sizeScore)
+
+
+func _on_restart_pressed():
+	var sceneManager : SceneManager = GlobalVars.sceneManager
+	sceneManager.currentScene = self
+	sceneManager.switchScene(self, mainMenu)
