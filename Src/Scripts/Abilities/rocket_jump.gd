3a13284242f575ccbde5415b043a04f32df92d40
awesomness560 <appsforyou6@gmail.com>
2024-05-31 21:15:07 -0500

    Started adding in rocket jump shotgun

---
+++ Src/Scripts/Abilities/rocket_jump.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@export var speed : float = 2
+@export var player : Player
+@export var damageModule : Damager
+@export var blastArea : Area2D
+
+func _ready():
+	speed *= 1000000
+	Signal_bus.overcharged.connect(onOvercharge)
+	Signal_bus.usedOvercharge.connect(offOvercharge)
+	
+	assert(player, "You have to assign the player to the Rocket Jump Node")
+
+func _unhandled_input(event):
+	if event.is_action_pressed("use_ability_2"):
+		explode()
+
+func explode():
+	Signal_bus.shakeCam.emit(40, 5)
+	applyVelocity()
+	
+	var bodies : Array = blastArea.get_overlapping_bodies()
+	
+	for body in bodies:
+		if not body is Player:
+			damageModule.dealDamage(body)
+
+func onOvercharge():
+	damageModule.critRate = 1
+
+func offOvercharge():
+	damageModule.critRate = 0
+
+func applyVelocity():
+	var vector := player.global_transform.origin - global_transform.origin
+	var direction := vector.normalized()
+	var distance := vector.length()
+	
+	var velocity := (direction * speed)/distance
+	
+	player.velocity += velocity
